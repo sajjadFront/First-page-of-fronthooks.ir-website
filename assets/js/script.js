1644f39b6a62575eeca4d9ac92bc03ca324ef7f0
sajjad.front <sajjad.front@gmail.com>
2022-01-08 23:06:42 +0330

    courses section styles added and responsived

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,29 @@
 let $ = document;
 
-const hamburgerIcon = $.querySelector('.header__hamburger-icon');
-const closeIcon = $.querySelector('.header__close-icon');
-const navbar = $.querySelector('.navbar');
+// header fixed top
+const header = $.querySelector("header");
+// show navbar
+const hamburgerIcon = $.querySelector(".header__hamburger-icon");
+const closeIcon = $.querySelector(".header__close-icon");
+const navbar = $.querySelector(".navbar");
 
 // Functions
-hamburgerIcon.addEventListener('click', ()=>{
-    hamburgerIcon.classList.toggle('active');
-    closeIcon.classList.toggle('active');
-    navbar.classList.toggle('active');
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 620) {
+    header.style.position = "fixed";
+  } else {
+    header.style.position = "sticky";
+  }
 });
 
-closeIcon.addEventListener('click', ()=>{
-    hamburgerIcon.classList.toggle('active');
-    closeIcon.classList.toggle('active');
-    navbar.classList.toggle('active');
+hamburgerIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.toggle("active");
+  closeIcon.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
+
+closeIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.toggle("active");
+  closeIcon.classList.toggle("active");
+  navbar.classList.toggle("active");
 });

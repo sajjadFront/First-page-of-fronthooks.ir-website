619e87e9e74ba7b47afd9e701bffc377d79ffaf5
sajjad.front <sajjad.front@gmail.com>
2022-01-09 10:52:38 +0330

    Extras removed from script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,9 @@
 let $ = document;
 
-// header fixed top
-const header = $.querySelector("header");
 // show navbar
 const hamburgerIcon = $.querySelector(".header__hamburger-icon");
 const closeIcon = $.querySelector(".header__close-icon");
 const navbar = $.querySelector(".navbar");
-
-// Functions
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 620) {
-    header.style.position = "fixed";
-  } else {
-    header.style.position = "sticky";
-  }
-});
 
 hamburgerIcon.addEventListener("click", () => {
   hamburgerIcon.classList.toggle("active");

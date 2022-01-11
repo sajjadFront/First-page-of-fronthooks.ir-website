066a189eeddcede0fea72b5b862506f9799256ea
sajjadFront <sajjad.front@gmail.com>
2022-01-11 12:35:18 +0330

    primary color added to menu Items in javascript.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ let $ = document;
 const hamburgerIcon = $.querySelector(".header__hamburger-icon");
 const closeIcon = $.querySelector(".header__close-icon");
 const navbar = $.querySelector(".navbar");
+// click on menu item
+let menuItems = $.querySelectorAll("header .menu a");
+
+// Function
 
 hamburgerIcon.addEventListener("click", () => {
   hamburgerIcon.classList.toggle("active");
@@ -16,3 +20,14 @@ closeIcon.addEventListener("click", () => {
   closeIcon.classList.toggle("active");
   navbar.classList.toggle("active");
 });
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", colorFunc);
+});
+
+function colorFunc() {
+  menuItems.forEach((menuItem) => {
+    menuItem.classList.remove("active");
+    this.classList.add("active");
+  });
+}
